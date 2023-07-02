5d2628655a2763288746c23cdb40e58f2682b417
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-07-02 03:51:23 -0400

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Lori Rothermel on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
